0b88a26d24348fae8cfda834517458d0d54842f0
Ivy Xu <xyan21418@gmail.com>
2024-10-31 23:45:23 +0200

    set up redis cache for wordpress website; fixed permission issues under /var/www/html in wordpress container

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -9,21 +9,38 @@ done
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     echo "WordPress files not found. Installing WordPress..."
-
     echo "Downloading WordPress..."
     wp core download --allow-root
+
+    # basic database setup
+    echo "Configuring wp-config.php..."
     wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} \
       --dbpass=${DB_PASSWORD} --dbhost=${DB_HOSTNAME} --allow-root
+    wp config set WP_REDIS_HOST 'redis' --allow-root
+    wp config set WP_REDIS_PORT 6379 --raw --allow-root
+    wp config set WP_CACHE true --raw --allow-root
+
+    # Redis cache settings
     echo "Installing WordPress..."
     wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} \
       --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+
     echo "Creating an user..."
     wp user create ${WP_USER} ${WP_USER_EMAIL} --role=author \
       --user_pass=${WP_USER_PWD} --allow-root
-    echo "Installing theme GeneratePress..."
+
     wp theme install generatepress --activate --allow-root
+
+    # install and activate the Redis Object Cache plugin
+    wp plugin install redis-cache --activate --allow-root
+
+    # enable object caching within WordPress
+    wp redis enable --allow-root
+
 else
     echo "WordPress files already present. Skipping installation."
 fi
+
+chown -R www-data:www-data /var/www/html
 
 php-fpm7.4 -F
